62497861f8ac2f9310c218ad9ea1f840055e2acd
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-07 21:39:08 +0530

    Create checker.h

---
+++ checker.h
@@ -0,0 +1,20 @@
+#define TemperatureMinLimit 0
+#define TemperatureMaxLimit 45
+#definr SOCMinLimit 20
+#define SOCMaxLimit 80
+#define ChargeRateMinLimit 0.0
+#define ChargeRateMaxLimit 0.8
+
+typedef enum {
+  TempParameter,
+  SOCParameter,
+  ChargeRateParameter,
+  MaxParameter
+} parameterList;
+
+typedef struct {
+  parameterList parameterName,
+  int minThreshold,
+  int maxThreshold,
+  char msgInput[20]
+} parameterInfo;
