6efe0cec16bcc106059f675af68db457c6c64479
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-09 23:02:45 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -20,7 +20,7 @@ typedef struct {
 } ParameterInfo;
 
 void printOnConsole(const char msg[]);
-int isParametersWithinRange (const parameterDetails[], const double testData[], void (*Fn_Ptr_WarningMsg)(const char));
+int isParametersWithinRange (const ParameterInfo parameterDetails[], const double testData[], void (*Fn_Ptr_WarningMsg)(const char));
 void testBattery(int isBatteryOK, int expectedResult);
 //int isParametersWithinRange (ParameterList parametersName, double inputValue);
 //void testBattery(double testData[], int expectedResult);
