7a6ba0d8c48675b287632f65940b8d820523337f
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-09 23:38:39 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,49 +2,44 @@
 #include <assert.h>
 #include "checker.h"
 
-ParameterInfo parameterInfo [MaxParameter] = {
+const ParameterInfo parameterInfo [MaxParameter] = {
   {TempParameter, TemperatureMinLimit, TemperatureMaxLimit, "Temperature"},
   {SOCParameter, SOCMinLimit, SOCMaxLimit, "State of Charge" },
   {ChargeRateParameter, ChargeRateMinLimit, ChargeRateMaxLimit, "Charge Rate"}
 };
 
-int isParametersWithinRange (ParameterInfo parameterDetails[], double testData[], void (*Fn_Ptr_WarningMsg)(char[])) {
-  int result = 1;
-  for(int i=0; i< MaxParameter; i++) {
-      if(testData[i] < parameterDetails[i].minThreshold || testData[i] > parameterDetails[i].maxThreshold) {
-          Fn_Ptr_WarningMsg(parameterDetails[i].msgInput);
-          result &= 0;
-      } else{
-          result &= 1;
-      }
+int isParametersWithinRange (ParameterList parametersName, double inputValue) {
+  if(inputValue < parameterInfo[parametersName].minThreshold || inputValue > parameterInfo[parametersName].maxThreshold) {
+      printOnConsole(parameterInfo[parametersName].msgInput);
+      return 0;
   }
-  return result;
+  return 1;
 }
 
-void printOnConsole(char msg[]) {
+void printOnConsole(const char msg[]) {
     printf("%s out of range!\n",msg);
 }
 
-void testBattery(int isBatteryOK, int expectedResult) {
-  assert(isBatteryOK == expectedResult);
+void testBattery(double testData[], int expectedResult) {
+  int result = 1;
+  for(int i=0; i< MaxParameter; i++) {
+      result &= isParametersWithinRange(parameterInfo[i].parameterName, testData[i]);
+  }
+  assert(result == expectedResult);
 }
 
 int main() {
-  void (*Fn_Ptr_WarningMsg)(char[]);
-  Fn_Ptr_WarningMsg = &printOnConsole;
   double testData1[] = {25,70,0.7};
-  int isBatteryOK = isParametersWithinRange(parameterInfo, testData1, Fn_Ptr_WarningMsg);
-  testBattery(isBatteryOK, 1);
-//   testBattery(testData1, 1);
-//   double testData2[] = {50,85,0.9};
-//   testBattery(testData2, 0);
-//   double testData3[] = {46,70,0.7};
-//   testBattery(testData3, 0);
-//   double testData4[] = {25,85,0.7};
-//   testBattery(testData4, 0);
-//   double testData5[] = {25,70,0.9};
-//   testBattery(testData5, 0);
-//   double testData6[] = {-10,70,0.9};
-//   testBattery(testData6, 0);
+  testBattery(testData1, 1);
+  double testData2[] = {50,85,0.9};
+  testBattery(testData2, 0);
+  double testData3[] = {46,70,0.7};
+  testBattery(testData3, 0);
+  double testData4[] = {25,85,0.7};
+  testBattery(testData4, 0);
+  double testData5[] = {25,70,0.9};
+  testBattery(testData5, 0);
+  double testData6[] = {-10,70,0.9};
+  testBattery(testData6, 0);
   return 0;
 }
