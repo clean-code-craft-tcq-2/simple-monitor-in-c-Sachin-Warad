b29ed69dfac2914a677363ee57a43811415532e4
Sachin-Warad <91026113+Sachin-Warad@users.noreply.github.com>
2022-02-16 06:59:10 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -32,7 +32,9 @@ typedef struct {
 } ParameterInfo;
 
 void printOnConsole(char msg[]);
-void testBattery(ParameterInfo parameterDetails[], double testData[], int isWarningRequired[], int expectedResult);
+void testBattery(ParameterInfo parameterDetails[], double testData[], int isWarningRequired[], int expectedResult, char* temperatureUnit);
 int isParametersWithinRange (ParameterInfo parameterDetails, double inputValue, void (*Fn_Ptr_WarningMsg)(char[]));
 int isParametersWithingWarningRange (ParameterInfo parameterDetails, double inputValue, void (*Fn_Ptr_WarningMsg)(char[]), int isWarningRequired);
 int evaluateWarningRange(ParameterInfo parameterDetails, double inputValue);
+void convertTemperatureToCelcius(char* temperatureUnit, double testData[]);
+void convertFarenheitToCelcius(double testData[]);
